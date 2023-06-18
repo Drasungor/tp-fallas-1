6b92d5b4551fb79c893ccb8ae8e07e171793fc1c
Agustin <aguscambiano@gmail.com>
2023-06-18 11:37:22 -0300

    Delete old code

--- Back/main.py
+++ Back/main.py
@@ -27,14 +27,6 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-def create_rule(input_1, input_2, input_3, input_4, input_5):
-    # CUIDADO, lo que es texto tiene que tener el mismo nombre que los argumentos del body, sino tira una exception gigante
-    rule = Rule(f"input_1 == {str(input_1).lower()} and input_2 == {str(input_2).lower()} and input_3 == {str(input_3).lower()} and input_4 == {str(input_4).lower()} and input_5 == {str(input_5).lower()} ")
-    return rule
-
-# aca tenemos que agregar todas las rules que creamos. La primera parte de la tupla, la funcion, crea la regla que se cumple si lo que 
-# nos pasan de front es igual que los args. La segunda de la tupla parte es lo que le devolvemos al cliente si se cumple esa regla, osea la E elegida
-
 def stringifyBools(boolsArray):
     returnedString = str(boolsArray[0])
     for value in boolsArray[1:]:
@@ -100,37 +92,14 @@ async def home():
         status_code=200,
         content={'hello': 'get'})
 
-# Para chequear que esten todos los args del body del post, hay que ponerle nombres decentes y hacer que queden iguales en create_rule
-body_args = ["input_1", "input_2", "input_3", "input_4", "input_5"]
-
 @app.post("/")
-# async def get_fix(request: Request):
 async def get_fix(answersBody: Answers):
     print("POST request received at '/'")
-    # request_body = await request.json()
-    # print(request_body)
     answersDict = answersBody.dict()
     answersArray = answersDict["answers"]
     print(f"answersArray: {answersArray}")
     booleanValues = [answer ["answer"] for answer in answersArray]
     print(f"booleanValues: {booleanValues}")
-    # for attr in body_args:
-    #     if attr not in request_body:
-    #         return JSONResponse(
-    #                             status_code=400,
-    #                             content={'error': f"Attribute '{attr}' should be included in the request body"})
-    #     if not isinstance(request_body[attr], bool):
-    #         return JSONResponse(
-    #                             status_code=400,
-    #                             content={'error': f"Attribute '{attr}' should have a boolean value"})
-    # for rule in rules:
-    #     rule_func = rule[0]
-    #     output = rule[1]
-    #     result = rule_func.matches(request_body)
-    #     if (result):
-    #         return JSONResponse(
-    #                             status_code=200,
-    #                             content={"fix": output})
     return JSONResponse(
         status_code=200,
         content={"fix": "none"}) # Rta si no cumple ninguna regla
